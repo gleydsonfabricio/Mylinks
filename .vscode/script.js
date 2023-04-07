f6478d317687e684bb9582022352eb1233e5690e
Gleydson Fabricio <gleydsonfabricio3@gmail.com>
2023-04-07 15:34:29 -0300

    first initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const hmtl = document.documentElement
+  
+  //if(hmtl.classList.contains('light')) {
+  //  html.classList.remove('light')
+  //} else{
+  //  hmtl.classList.add('light')
+  //}
+  
+  // resumidamente ->
+
+  hmtl.classList.toggle('light')
+}
